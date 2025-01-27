35645338354525ff7e966964171c503f37813a25
gboccazzi_meli <gianni.boccazzi@mercadolibre.com>
2025-01-27 16:59:15 -0300

    :art: add router and loader

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/pantunezmeli/bootcamp-wave15-g7/cmd/server"
+)
+
+
+func main() {
+	// env
+	// ...
+
+	// app
+	// - config
+	cfg := &server.ConfigServerChi{
+		ServerAddress:  ":8080",
+		LoaderFilePath: "",
+	}
+	app := server.NewServerChi(cfg)
+	// - run
+	if err := app.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
--- cmd/server/app.go
+++ cmd/server/app.go
@@ -1,1 +1,101 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// ConfigServerChi is a struct that represents the configuration for ServerChi
+type ConfigServerChi struct {
+	// ServerAddress is the address where the server will be listening
+	ServerAddress string
+	// LoaderFilePath is the path to the file that contains the vehicles
+	LoaderFilePath string
+}
+
+// NewServerChi is a function that returns a new instance of ServerChi
+func NewServerChi(cfg *ConfigServerChi) *ServerChi {
+	// default values
+	defaultConfig := &ConfigServerChi{
+		ServerAddress: ":8080",
+	}
+	if cfg != nil {
+		if cfg.ServerAddress != "" {
+			defaultConfig.ServerAddress = cfg.ServerAddress
+		}
+		if cfg.LoaderFilePath != "" {
+			defaultConfig.LoaderFilePath = cfg.LoaderFilePath
+		}
+	}
+
+	return &ServerChi{
+		serverAddress:  defaultConfig.ServerAddress,
+		loaderFilePath: defaultConfig.LoaderFilePath,
+	}
+}
+
+// ServerChi is a struct that implements the Application interface
+type ServerChi struct {
+	// serverAddress is the address where the server will be listening
+	serverAddress string
+	// loaderFilePath is the path to the file that contains the vehicles
+	loaderFilePath string
+}
+
+// Run is a method that runs the server
+func (a *ServerChi) Run() (err error) {
+	// dependencies
+	// - loader
+	// ld := loader.NewVehicleJSONFile(a.loaderFilePath)
+	// db, err := ld.Load()
+	// if err != nil {
+	// 	return
+	// }
+
+
+	// - repository
+
+
+
+	// - service
+
+
+	// - handler
+
+
+	// router
+	rt := chi.NewRouter()
+	// - middlewares
+	rt.Use(middleware.Logger)
+	rt.Use(middleware.Recoverer)
+	// - endpoints
+	rt.Route("/sellers", func(rt chi.Router) {
+
+	})
+
+	rt.Route("/warehouses", func(rt chi.Router) {
+
+	})
+
+	rt.Route("/sections", func(rt chi.Router) {
+
+	})
+
+	rt.Route("/products", func(rt chi.Router) {
+
+	})
+
+	rt.Route("/employees", func(rt chi.Router) {
+
+	})
+
+	rt.Route("/buyers", func(rt chi.Router) {
+
+	})
+
+	// run server
+	err = http.ListenAndServe(a.serverAddress, rt)
+	return
+}

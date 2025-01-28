4b03137820ab6abbebfc27f058f77e99816ec520
gboccazzi_meli <gianni.boccazzi@mercadolibre.com>
2025-01-28 15:55:45 -0300

    :sparkles: add seller service

--- internal/service/seller/service_seller.go
+++ internal/service/seller/service_seller.go
@@ -1,1 +1,11 @@
 package seller
+
+import "github.com/pantunezmeli/bootcamp-wave15-g7/internal/repository/seller"
+
+type SellerDefault struct {
+	rp seller.SellerRepository
+}
+
+func NewSellerDefault(rp seller.SellerRepository) *SellerDefault {
+	return &SellerDefault{rp}
+}

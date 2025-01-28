b09c32442b2748b6112b1a86383ffef317b13948
gboccazzi_meli <gianni.boccazzi@mercadolibre.com>
2025-01-28 12:52:29 -0300

    :sparkles: add seller model and dto

---
+++ pkg/dto/response_dto.go
@@ -0,0 +1,10 @@
+package dto
+
+type SellerDoc struct {
+	ID  int `json:"id"`
+	Cid int `json:"cid"`
+	CompanyName string `json:"company_name"`
+	Address string `json:"address"`
+	Telephone string `json:"telephone"`
+	
+}

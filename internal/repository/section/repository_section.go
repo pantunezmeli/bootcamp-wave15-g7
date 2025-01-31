857dfc60e2d2e7a4bef1e9021e388c185763e94a
Salvadroid <salvadoryagusz@gmail.com>
2025-01-31 08:26:30 -0300

    :goal_net: Errors Captured

--- internal/repository/section/repository_section.go
+++ internal/repository/section/repository_section.go
@@ -1,8 +1,6 @@
 package section
 
 import (
-	"errors"
-
 	"dario.cat/mergo"
 	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/models"
 	sectionstorage "github.com/pantunezmeli/bootcamp-wave15-g7/internal/storage/section"
@@ -24,7 +22,11 @@ func NewStRepository(storage sectionstorage.ISectionLoader) *StRepository {
 
 // FindAll is a method that returns anRepository of all Sections
 func (r *StRepository) FindAll() (map[int]models.Section, error) {
-	sections, _ := r.storage.Load()
+	sections, err := r.storage.Load()
+	if err != nil {
+		return nil, err
+	}
+
 	list := make(map[int]models.Section)
 	for key, value := range sections {
 		list[key] = value
@@ -37,26 +39,37 @@ func (r *StRepository) FindAll() (map[int]models.Section, error) {
 func (r *StRepository) FindByID(id int) (entity models.Section, err error) {
 	data, err := r.storage.Load()
 	if err != nil {
-		return models.Section{}, errors.New("section not found")
+		return models.Section{}, errorbase.ErrStorageOperationFailed
 	}
 
 	entity, exists := data[id]
 	if !exists {
-		return models.Section{}, errors.New("section not found")
+		return models.Section{}, errorbase.ErrNotFound
 	}
 	return entity, nil
 }
 
 // Create is a method that creates a new Section
 func (r *StRepository) Create(entity models.Section) (models.Section, error) {
-	data, err := r.storage.Load()
+
+	// Load all sections
+	sections, err := r.storage.Load()
 	if err != nil {
-		return models.Section{}, err
+		return models.Section{}, errorbase.ErrStorageOperationFailed
 	}
-	if _, exists := data[entity.Id]; exists {
-		return models.Section{}, errors.New("section already exists")
+
+	// Check if section number already exists
+	for _, section := range sections {
+		if section.Section_Number == entity.Section_Number {
+			return models.Section{}, errorbase.ErrConflict
+		}
 	}
-	data[entity.Id] = entity
+
+	// Set new ID
+	entity.Id = getLastId(sections)
+	sections[entity.Id] = entity
+
+	// Save new section
 	if err := r.storage.Save(entity); err != nil {
 		return models.Section{}, err
 	}
@@ -65,40 +78,56 @@ func (r *StRepository) Create(entity models.Section) (models.Section, error) {
 
 // Patch is a method that updates a Section by its ID
 func (r *StRepository) Update(id int, entity models.Section) (models.Section, error) {
+	// Load all sections
 	data, err := r.storage.Load()
 	if err != nil {
-		return models.Section{}, err
+		return models.Section{}, errorbase.ErrStorageOperationFailed
 	}
 
+	// Check if section exists
 	element, ok := data[id]
 	if !ok {
-		return models.Section{}, errorbase.ErrConflict
+		return models.Section{}, errorbase.ErrNotFound
 	}
 
+	// Merge the new data with the existing data
 	if err := mergo.Merge(&element, entity, mergo.WithOverride); err != nil {
 		return models.Section{}, err
 	}
 
 	data[id] = element
 	if err := r.storage.Save(data[id]); err != nil {
-		return models.Section{}, err
+		return models.Section{}, errorbase.ErrStorageOperationFailed
 	}
 	return element, nil
 }
 
 // Delete is a method that deletes a Section by its ID
 func (r *StRepository) Delete(id int) (err error) {
-	sections, _ := r.storage.Load()
+	sections, err := r.storage.Load()
+	if err != nil {
+		return errorbase.ErrStorageOperationFailed
+	}
 
 	sectionDelete, err := r.FindByID(id)
-
 	if err != nil {
-		return err
+		return errorbase.ErrNotFound
 	}
+
 	delete(sections, sectionDelete.Id)
-	err2 := r.storage.Delete(id)
-	if err2 != nil {
+	err = r.storage.Delete(id)
+	if err != nil {
 		return errorbase.ErrStorageOperationFailed
 	}
 	return nil
+}
+
+func getLastId(section map[int]models.Section) int {
+	maxId := 0
+	for id := range section {
+		if id > maxId {
+			maxId = id
+		}
+	}
+	return maxId + 1
 }

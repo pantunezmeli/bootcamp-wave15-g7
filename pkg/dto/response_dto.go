1dfa68fee77fc04d3cdd00d7182baf589bbb5d88
pabantunez_meli <pablo.antunez@mercadolibre.com>
2025-01-31 15:15:46 -0300

    :bricks: Warehouse --> Move dto to warehouse folder

--- pkg/dto/response_dto.go
+++ pkg/dto/response_dto.go
@@ -1,8 +1,6 @@
 package dto
 
 import (
-	"fmt"
-
 	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/models"
 	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/value_objects"
 )
@@ -68,128 +66,129 @@ func EmployeeModelToDto(employee models.Employee) (dto EmployeeDoc) {
 	return
 }
 
-type WareHouseDoc struct {
-	Id                 int    `json:"id"`
-	WareHouseCode      string `json:"warehouse_code"`
-	Address            string `json:"address"`
-	Telephone          string `json:"telephone"`
-	MinimunCapacity    int    `json:"minimun_capacity"`
-	MinimunTemperature int    `json:"minimun_temperature"`
-}
+// ! ########################## Eliminar ############################
+// type WareHouseDoc struct {
+// 	Id                 int    `json:"id"`
+// 	WareHouseCode      string `json:"warehouse_code"`
+// 	Address            string `json:"address"`
+// 	Telephone          string `json:"telephone"`
+// 	MinimunCapacity    int    `json:"minimun_capacity"`
+// 	MinimunTemperature int    `json:"minimun_temperature"`
+// }
 
-func (w WareHouseDoc) ConvertToModel(req WareHouseDoc) (models.WareHouse, error) {
+// func (w WareHouseDoc) ConvertToModel(req WareHouseDoc) (models.WareHouse, error) {
 
-	wareHouseCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
-	if err != nil {
-		return models.WareHouse{}, fmt.Errorf("invalid warehouse code '%s': %w", req.WareHouseCode, err)
-	}
+// 	wareHouseCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
+// 	if err != nil {
+// 		return models.WareHouse{}, fmt.Errorf("invalid warehouse code '%s': %w", req.WareHouseCode, err)
+// 	}
 
-	address, err := value_objects.NewAddress(req.Address)
-	if err != nil {
-		return models.WareHouse{}, fmt.Errorf("invalid warehouse address '%s': %w", req.Address, err)
-	}
+// 	address, err := value_objects.NewAddress(req.Address)
+// 	if err != nil {
+// 		return models.WareHouse{}, fmt.Errorf("invalid warehouse address '%s': %w", req.Address, err)
+// 	}
 
-	telephone, err := value_objects.NewTelephone(req.Telephone)
-	if err != nil {
-		return models.WareHouse{}, fmt.Errorf("invalid warehouse telephone '%s': %w", req.Telephone, err)
-	}
+// 	telephone, err := value_objects.NewTelephone(req.Telephone)
+// 	if err != nil {
+// 		return models.WareHouse{}, fmt.Errorf("invalid warehouse telephone '%s': %w", req.Telephone, err)
+// 	}
 
-	minCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
-	if err != nil {
-		return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun capacity '%d': %w", req.MinimunCapacity, err)
-	}
+// 	minCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
+// 	if err != nil {
+// 		return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun capacity '%d': %w", req.MinimunCapacity, err)
+// 	}
 
-	minTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
-	if err != nil {
-		return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun temperature '%d': %w", req.MinimunTemperature, err)
-	}
+// 	minTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
+// 	if err != nil {
+// 		return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun temperature '%d': %w", req.MinimunTemperature, err)
+// 	}
 
-	wh := models.WareHouse{
-		WareHouseCode:      wareHouseCode,
-		Address:            address,
-		Telephone:          telephone,
-		MinimunCapacity:    minCapacity,
-		MinimunTemperature: minTemp,
-	}
-	return wh, nil
-}
+// 	wh := models.WareHouse{
+// 		WareHouseCode:      wareHouseCode,
+// 		Address:            address,
+// 		Telephone:          telephone,
+// 		MinimunCapacity:    minCapacity,
+// 		MinimunTemperature: minTemp,
+// 	}
+// 	return wh, nil
+// }
 
-func (w WareHouseDoc) ConvertToDTO(req models.WareHouse) (wh WareHouseDoc, err error) {
-	return WareHouseDoc{
-		Id:                 value_objects.Id.GetId(req.Id),                                                 // Convertir value object Id a int
-		WareHouseCode:      value_objects.WareHouseCode.GetWareHouseCode(req.WareHouseCode),                // Convertir value object WareHouseCode a string
-		Address:            value_objects.Address.GetAddress(req.Address),                                  // Convertir value object Address a string
-		Telephone:          value_objects.Telephone.GetTelephone(req.Telephone),                            // Convertir value object Telephone a string
-		MinimunCapacity:    value_objects.MinimunCapacity.GetMinimunCapacity(req.MinimunCapacity),          // Convertir value object MinimunCapacity a int
-		MinimunTemperature: value_objects.MinimunTemperature.GetMinimunTemperature(req.MinimunTemperature), // Convertir value object MinimunTemperature a int
-	}, nil
-}
+// func (w WareHouseDoc) ConvertToDTO(req models.WareHouse) (wh WareHouseDoc, err error) {
+// 	return WareHouseDoc{
+// 		Id:                 value_objects.Id.GetId(req.Id),                                                 // Convertir value object Id a int
+// 		WareHouseCode:      value_objects.WareHouseCode.GetWareHouseCode(req.WareHouseCode),                // Convertir value object WareHouseCode a string
+// 		Address:            value_objects.Address.GetAddress(req.Address),                                  // Convertir value object Address a string
+// 		Telephone:          value_objects.Telephone.GetTelephone(req.Telephone),                            // Convertir value object Telephone a string
+// 		MinimunCapacity:    value_objects.MinimunCapacity.GetMinimunCapacity(req.MinimunCapacity),          // Convertir value object MinimunCapacity a int
+// 		MinimunTemperature: value_objects.MinimunTemperature.GetMinimunTemperature(req.MinimunTemperature), // Convertir value object MinimunTemperature a int
+// 	}, nil
+// }
 
-func (w WareHouseDoc) ConvertToModelPatch(req WareHouseDoc, existingWarehouse models.WareHouse) (models.WareHouse, error) {
-	if req.WareHouseCode != "" {
-		newCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
-		if err != nil {
-			return models.WareHouse{}, fmt.Errorf("invalid warehouse code '%s': %w", req.WareHouseCode, err)
-		}
-		existingWarehouse.WareHouseCode = newCode
-	}
+// func (w WareHouseDoc) ConvertToModelPatch(req WareHouseDoc, existingWarehouse models.WareHouse) (models.WareHouse, error) {
+// 	if req.WareHouseCode != "" {
+// 		newCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
+// 		if err != nil {
+// 			return models.WareHouse{}, fmt.Errorf("invalid warehouse code '%s': %w", req.WareHouseCode, err)
+// 		}
+// 		existingWarehouse.WareHouseCode = newCode
+// 	}
 
-	if req.Address != "" {
-		newAddress, err := value_objects.NewAddress(req.Address)
-		if err != nil {
-			return models.WareHouse{}, fmt.Errorf("invalid warehouse address '%s': %w", req.Address, err)
-		}
-		existingWarehouse.Address = newAddress
-	}
-	if req.Telephone != "" {
-		newTelephone, err := value_objects.NewTelephone(req.Telephone)
-		if err != nil {
-			return models.WareHouse{}, fmt.Errorf("invalid warehouse telephone '%s': %w", req.Telephone, err)
-		}
-		existingWarehouse.Telephone = newTelephone
-	}
-	if req.MinimunCapacity > 0 {
-		newMinCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
-		if err != nil {
-			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun capacity '%d': %w", req.MinimunCapacity, err)
-		}
-		existingWarehouse.MinimunCapacity = newMinCapacity
-	}
+// 	if req.Address != "" {
+// 		newAddress, err := value_objects.NewAddress(req.Address)
+// 		if err != nil {
+// 			return models.WareHouse{}, fmt.Errorf("invalid warehouse address '%s': %w", req.Address, err)
+// 		}
+// 		existingWarehouse.Address = newAddress
+// 	}
+// 	if req.Telephone != "" {
+// 		newTelephone, err := value_objects.NewTelephone(req.Telephone)
+// 		if err != nil {
+// 			return models.WareHouse{}, fmt.Errorf("invalid warehouse telephone '%s': %w", req.Telephone, err)
+// 		}
+// 		existingWarehouse.Telephone = newTelephone
+// 	}
+// 	if req.MinimunCapacity > 0 {
+// 		newMinCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
+// 		if err != nil {
+// 			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun capacity '%d': %w", req.MinimunCapacity, err)
+// 		}
+// 		existingWarehouse.MinimunCapacity = newMinCapacity
+// 	}
 
-	if req.MinimunTemperature > -100 {
-		newMinTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
-		if err != nil {
-			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun temperature '%d': %w", req.MinimunTemperature, err)
-		}
-		existingWarehouse.MinimunTemperature = newMinTemp
-	}
-	return existingWarehouse, nil
-}
+// 	if req.MinimunTemperature > -100 {
+// 		newMinTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
+// 		if err != nil {
+// 			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun temperature '%d': %w", req.MinimunTemperature, err)
+// 		}
+// 		existingWarehouse.MinimunTemperature = newMinTemp
+// 	}
+// 	return existingWarehouse, nil
+// }
 
+//! #################################################################
 
 type SellerDoc struct {
-	ID  *int `json:"id"`
-	Cid *int `json:"cid"`
+	ID          *int    `json:"id"`
+	Cid         *int    `json:"cid"`
 	CompanyName *string `json:"company_name"`
-	Address *string `json:"address"`
-	Telephone *string `json:"telephone"`
+	Address     *string `json:"address"`
+	Telephone   *string `json:"telephone"`
 }
 
-
-func ParseModelToDto(sellerModel models.Seller) (sellerDto SellerDoc){
+func ParseModelToDto(sellerModel models.Seller) (sellerDto SellerDoc) {
 	sellerDto = SellerDoc{
-			ID: sellerModel.ID.Value(),
-			Cid: sellerModel.Cid.Value(),
-			CompanyName: sellerModel.CompanyName.Value(),
-			Address: sellerModel.Address.Value(),
-			Telephone: sellerModel.Telephone.Value(),
-		}
+		ID:          sellerModel.ID.Value(),
+		Cid:         sellerModel.Cid.Value(),
+		CompanyName: sellerModel.CompanyName.Value(),
+		Address:     sellerModel.Address.Value(),
+		Telephone:   sellerModel.Telephone.Value(),
+	}
 	return
 }
 
-func ParseDtoToModel(sellerDto SellerDoc) (sellerModel models.Seller, err error){
+func ParseDtoToModel(sellerDto SellerDoc) (sellerModel models.Seller, err error) {
 	cid, err := value_objects.NewCid(*sellerDto.Cid)
-	if err != nil{
+	if err != nil {
 		return
 	}
 	companyName, err := value_objects.NewCompanyName(*sellerDto.CompanyName)
@@ -197,7 +196,7 @@ func ParseDtoToModel(sellerDto SellerDoc) (sellerModel models.Seller, err error)
 		return
 	}
 	address, err := value_objects.NewSellerAddress(*sellerDto.Address)
-	if err != nil{
+	if err != nil {
 		return
 	}
 	telephone, err := value_objects.NewSellerTelephone(*sellerDto.Telephone)
@@ -207,12 +206,11 @@ func ParseDtoToModel(sellerDto SellerDoc) (sellerModel models.Seller, err error)
 
 	sellerModel = models.Seller{
 		SellerAttributes: models.SellerAttributes{
-			Cid: cid,
+			Cid:         cid,
 			CompanyName: companyName,
-			Address: address,
-			Telephone: telephone,
+			Address:     address,
+			Telephone:   telephone,
 		},
 	}
 	return
 }
-
---
+++ pkg/dto/warehouse/warehouse_dto.go
@@ -0,0 +1,106 @@
+package warehouse
+
+import (
+	"fmt"
+
+	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/models"
+	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/value_objects"
+)
+
+type WareHouseDoc struct {
+	Id                 int    `json:"id"`
+	WareHouseCode      string `json:"warehouse_code"`
+	Address            string `json:"address"`
+	Telephone          string `json:"telephone"`
+	MinimunCapacity    int    `json:"minimun_capacity"`
+	MinimunTemperature int    `json:"minimun_temperature"`
+}
+
+func (w WareHouseDoc) ConvertToModel(req WareHouseDoc) (models.WareHouse, error) {
+
+	wareHouseCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
+	if err != nil {
+		return models.WareHouse{}, err
+	}
+
+	address, err := value_objects.NewAddress(req.Address)
+	if err != nil {
+		return models.WareHouse{}, err
+	}
+
+	telephone, err := value_objects.NewTelephone(req.Telephone)
+	if err != nil {
+		return models.WareHouse{}, err
+	}
+
+	minCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
+	if err != nil {
+		return models.WareHouse{}, err
+	}
+
+	minTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
+	if err != nil {
+		return models.WareHouse{}, err
+	}
+
+	wh := models.WareHouse{
+		WareHouseCode:      wareHouseCode,
+		Address:            address,
+		Telephone:          telephone,
+		MinimunCapacity:    minCapacity,
+		MinimunTemperature: minTemp,
+	}
+	return wh, nil
+}
+
+func (w WareHouseDoc) ConvertToDTO(req models.WareHouse) (wh WareHouseDoc, err error) {
+	return WareHouseDoc{
+		Id:                 value_objects.Id.GetId(req.Id),                                                 // Convertir value object Id a int
+		WareHouseCode:      value_objects.WareHouseCode.GetWareHouseCode(req.WareHouseCode),                // Convertir value object WareHouseCode a string
+		Address:            value_objects.Address.GetAddress(req.Address),                                  // Convertir value object Address a string
+		Telephone:          value_objects.Telephone.GetTelephone(req.Telephone),                            // Convertir value object Telephone a string
+		MinimunCapacity:    value_objects.MinimunCapacity.GetMinimunCapacity(req.MinimunCapacity),          // Convertir value object MinimunCapacity a int
+		MinimunTemperature: value_objects.MinimunTemperature.GetMinimunTemperature(req.MinimunTemperature), // Convertir value object MinimunTemperature a int
+	}, nil
+}
+
+func (w WareHouseDoc) ConvertToModelPatch(req WareHouseDoc, existingWarehouse models.WareHouse) (models.WareHouse, error) {
+	if req.WareHouseCode != "" {
+		newCode, err := value_objects.NewWareHouseCode(req.WareHouseCode)
+		if err != nil {
+			return models.WareHouse{}, fmt.Errorf("invalid warehouse code '%s': %w", req.WareHouseCode, err)
+		}
+		existingWarehouse.WareHouseCode = newCode
+	}
+
+	if req.Address != "" {
+		newAddress, err := value_objects.NewAddress(req.Address)
+		if err != nil {
+			return models.WareHouse{}, fmt.Errorf("invalid warehouse address '%s': %w", req.Address, err)
+		}
+		existingWarehouse.Address = newAddress
+	}
+	if req.Telephone != "" {
+		newTelephone, err := value_objects.NewTelephone(req.Telephone)
+		if err != nil {
+			return models.WareHouse{}, fmt.Errorf("invalid warehouse telephone '%s': %w", req.Telephone, err)
+		}
+		existingWarehouse.Telephone = newTelephone
+	}
+	if req.MinimunCapacity > 0 {
+		newMinCapacity, err := value_objects.NewMinimunCapacity(req.MinimunCapacity)
+		if err != nil {
+			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun capacity '%d': %w", req.MinimunCapacity, err)
+		}
+		existingWarehouse.MinimunCapacity = newMinCapacity
+	}
+
+	if req.MinimunTemperature > -100 {
+		newMinTemp, err := value_objects.NewMinimunTemperature(req.MinimunTemperature)
+		if err != nil {
+			return models.WareHouse{}, fmt.Errorf("invalid warehouse minimun temperature '%d': %w", req.MinimunTemperature, err)
+		}
+		existingWarehouse.MinimunTemperature = newMinTemp
+	}
+	return existingWarehouse, nil
+}

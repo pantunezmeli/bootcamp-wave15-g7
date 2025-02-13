2bc9c1270eca823f7b9b119dea1d1a1eff812517
Matias Agustin Romero <matias.romero@mercadolibre.com>
2025-02-13 14:03:11 -0300

    feat(product_records): add implement CreateProductRecord in  ProductRecordsService
    add parsers ValidAndParserDTO ParserRecordsToDto

--- internal/service/product_records/service_product_records.go
+++ internal/service/product_records/service_product_records.go
@@ -2,6 +2,8 @@ package product_records
 
 import (
 	"errors"
+	m "github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/models"
+	errdb "github.com/pantunezmeli/bootcamp-wave15-g7/internal/repository/product/errordb"
 	rp "github.com/pantunezmeli/bootcamp-wave15-g7/internal/repository/productrecords"
 	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/service/product/errsv"
 	dto "github.com/pantunezmeli/bootcamp-wave15-g7/pkg/dto/productrecords"
@@ -15,9 +17,25 @@ func NewProductRecordsService(rp rp.IProductRecordsRepository) *ProductRecordsSe
 	return &ProductRecordsService{rp: rp}
 }
 
-func (p ProductRecordsService) CreateProductRecord(newProductRecord dto.ProductRecordsDto) (dto.ProductRecordsDto, error) {
-	//TODO implement me
-	panic("implement me")
+func (p ProductRecordsService) CreateProductRecord(newProductRecord dto.ProductRecordsDto) (recordDto dto.ProductRecordsDto, err error) {
+	var newRecord m.ProductRecords
+	errValid := dto.ValidAndParserDTO(newProductRecord, &newRecord)
+	if errValid != nil {
+		err = errsv.ErrValidEntity{Message: errValid.Error()}
+		return
+	}
+
+	if errCreate := p.rp.CreateProductRecord(&newRecord); errCreate != nil {
+		if errors.As(errCreate, &errdb.ErrViolateFK{}) {
+			err = errsv.ErrConflict{Message: errCreate.Error()}
+			return
+		}
+		err = errsv.ErrService{Message: "Error creating Product Record"}
+		return
+	}
+
+	recordDto = dto.ParserRecordsToDto(newRecord)
+	return
 }
 
 func (p ProductRecordsService) GetProductRecord(productID *int) (recordsDto []dto.RecordsResponse, err error) {
--- pkg/dto/productrecords/product_records.go
+++ pkg/dto/productrecords/product_records.go
@@ -1,5 +1,14 @@
 package productrecords
 
+import (
+	m "github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/models"
+	vo "github.com/pantunezmeli/bootcamp-wave15-g7/internal/domain/value_objects"
+	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/service/product/errsv"
+	"time"
+)
+
+const LAYOUT_DATE = "2006-01-02"
+
 type Data struct {
 	Data ProductRecordsDto
 }
@@ -9,4 +18,40 @@ type ProductRecordsDto struct {
 	PurchasePrice  float64 `json:"purchase_price"`
 	SalePrice      float64 `json:"sale_price"`
 	ProductID      int     `json:"product_id"`
+}
+
+func ValidAndParserDTO(dto ProductRecordsDto, record *m.ProductRecords) error {
+	id, err := vo.NewId(dto.ProductID)
+	if err != nil {
+		return errsv.ErrValidEntity{Message: "Invalid product id"}
+	}
+
+	if dto.LastUpdateDate == "" {
+		return errsv.ErrValidEntity{Message: "Invalid last update date"}
+	}
+
+	if dto.PurchasePrice <= 0 || dto.SalePrice <= 0 {
+		return errsv.ErrValidEntity{Message: "Invalid price"}
+	}
+
+	date, err := time.Parse(LAYOUT_DATE, dto.LastUpdateDate)
+	if err != nil {
+		return errsv.ErrValidEntity{Message: "Invalid date"}
+	}
+
+	record.LastUpdateDate = date
+	record.PurchasePrice = dto.PurchasePrice
+	record.SalePrice = dto.SalePrice
+	record.ProductId = id
+
+	return nil
+}
+
+func ParserRecordsToDto(record m.ProductRecords) ProductRecordsDto {
+	return ProductRecordsDto{
+		LastUpdateDate: record.LastUpdateDate.Format("2006-01-02"),
+		PurchasePrice:  record.PurchasePrice,
+		SalePrice:      record.SalePrice,
+		ProductID:      record.ProductId.GetId(),
+	}
 }

6ed0342e2d641309acf3b03e543ba4b73228bd95
Matias Agustin Romero <matias.romero@mercadolibre.com>
2025-02-13 12:54:13 -0300

    feat(product_records) Implement GetRecords in HandlerProductRecords

--- internal/handler/handler_products_records.go
+++ internal/handler/handler_products_records.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"github.com/bootcamp-go/web/response"
 	"github.com/pantunezmeli/bootcamp-wave15-g7/internal/service/product_records"
+	"github.com/pantunezmeli/bootcamp-wave15-g7/pkg/dto"
 	"net/http"
+	"strconv"
 )
 
 type HandlerProductRecords struct {
@@ -22,7 +25,25 @@ func (h HandlerProductRecords) Create() http.HandlerFunc {
 
 func (h HandlerProductRecords) GetRecords() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		//TODO implement me
-		panic("implement me")
+		var idParam *int
+		idPath := r.URL.Query().Get("id")
+		if idPath != "" {
+			idInt, err := strconv.Atoi(idPath)
+			if err != nil {
+				dto.JSONError(w, http.StatusBadRequest, ErrInvalidId.Error())
+				return
+			}
+			idParam = &idInt
+		} else {
+			idParam = nil
+		}
+
+		productSearch, errSearch := h.sv.GetProductRecord(idParam)
+		if errSearch != nil {
+			validErrorResponse(w, errSearch)
+			return
+		}
+
+		response.JSON(w, http.StatusOK, dto.GenericResponse{Data: productSearch})
 	}
 }
